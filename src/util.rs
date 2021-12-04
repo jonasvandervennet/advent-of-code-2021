d4c2e2ddb942914f125f1ff1685481fbfd90b08d
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2021-12-04 09:23:46 +0100

    Clean util file

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,4 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 use std::time::Duration;
-
-// https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
 
 fn print_answer(part_nr: usize, answer: &str, known_answer: &str, duration: Duration) {
     if answer != known_answer {
